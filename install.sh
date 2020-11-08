f74040b4523011f2940d7bae196abc0ed54c2bc3
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2020-11-08 18:09:16 +0000

    ZSH plugins are not installed through homebrew

--- install.sh
+++ install.sh
@@ -10,57 +10,42 @@ xcode-select --install
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Install essential libraries
-brew install graphviz
-
-# Install languages
-brew install node
-brew install rbenv
-brew install python
-brew install go
-brew install elixir
-
-# Install editors and tools
-brew install vim
+# Install the essentials
 brew install bat
-brew install tree
-brew install exercism
+brew install elixir
+brew install go
+brew install gotop
+brew install graphviz
 brew install http-server
-
-# Install ZSH plugins
-brew install zsh-autosuggestions
-brew install zsh-completions
-brew install zsh-navigation-tools
-brew install zsh-syntax-highlighting
+brew install lsd
+brew install neofetch
+brew install node
+brew install python
+brew install rbenv
+brew install tree
+brew install vim
 
 # Enable Homebrew Casks
 brew install caskroom/cask
 
-# Install applications
+# Install applications and tools
 brew cask install --appdir="/Applications" docker
+brew cask install --appdir="/Applications" dotnet
 brew cask install --appdir="/Applications" expressvpn
 brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" iterm2
+brew cask install --appdir="/Applications" microsoft-teams
 brew cask install --appdir="/Applications" paw
 brew cask install --appdir="/Applications" postgres
 brew cask install --appdir="/Applications" postico
-brew cask install --appdir="/Applications" vlc
+brew cask install --appdir="/Applications" rectangle
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" steam
 brew cask install --appdir="/Applications" transmission
-
-# RU
+brew cask install --appdir="/Applications" visual-studio-code
+brew cask install --appdir="/Applications" vlc
 brew cask install --appdir="/Applications" zoomus
-
-# Annata stuff
-brew cask install --appdir="/Applications" dotnet
-brew cask install --appdir="/Applications" yammer
-brew cask install --appdir="/Applications" microsoft-teams
-
-# Visual Studio Code Insiders
-brew tap homebrew/cask-versions
-brew cask install --appdir="/Applications" visual-studio-code-insiders
 
 # Install fonts
 brew tap homebrew/cask-fonts
@@ -68,6 +53,7 @@ brew cask install font-fira-code
 brew cask install font-inconsolata
 brew cask install font-roboto-mon
 brew cask install font-input
+brew cask install font-hack-nerd-font
 
 # Set macOS defaults
 # Disable annoying keyboard features
@@ -76,28 +62,24 @@ defaults write -g NSAutomaticCapitalizationEnabled -bool false
 defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
 defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write -g NSSpellCheckerAutomaticallyIdentifiesLanguages -bool false
-
-# Font rendering on non-retina sucks
-defaults write -g CGFontRenderingFontSmoothingDisabled -bool false
-defaults write -g AppleFontSmoothing -int 1
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Automatic dark/light mode
 defaults write -g AppleInterfaceStyleSwitchesAutomatically -bool true
 
-# defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.finder CreateDesktop false
-# to revert all dock settings: defaults delete com.apple.dock; killall Dock
 defaults write com.apple.dock mineffect -string scale
 defaults write com.apple.dock tilesize -integer 48
 defaults write com.apple.dock autohide-time-modifier -float 0
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock autohide -bool true
+# to revert all dock settings: defaults delete com.apple.dock; killall Dock
 
 killall Dock
 killall Finder
 
 # Install global NPM packages
-npm install -g ncu
+npm install -g npm-check-updates
 npm install -g jshint
 
 # Configure Exercism
@@ -109,6 +91,10 @@ cp DefaultKeyBinding.dict ~/Library/KeyBindings/
 
 # Copy custom themes
 cp themes/codehugger.zsh-theme ~/.oh-my-zsh/custom/themes/codehugger.zsh-theme
+
+# Install oh-my-zsh plugins
+git clone https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Symlink dotfiles
 ./makesymlinks.sh
